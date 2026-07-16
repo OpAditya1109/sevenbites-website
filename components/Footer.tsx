0e830572a5d6d720324dc369544c181975b4ba91
Aditya Yadav <adityay1109@gmail.com>
2026-07-16 18:49:02 +0530

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
     return null;
   }
 
-  
+
   return (
     <footer className="bg-gray-950 text-gray-400">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-8">
@@ -43,7 +43,7 @@ export default function Footer() {
           {/* Brand */}
           <div className="lg:col-span-2">
             <Link href="/" className="flex items-center gap-2 mb-4">
-              <img src="/logo.png" alt="SevenBites" className="w-9 h-9" />
+              <img src="/icon.png" alt="SevenBites" className="w-9 h-9" />
               <span className="font-poppins font-bold text-2xl text-white">
                 Seven<span className="text-[#E23744]">Bites</span>
               </span>
